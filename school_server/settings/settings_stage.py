c56152454d40b11fc4904ba11469d3a12e200373
Deepak Prajapati <Dee83820@gmail.com>
2024-01-24 00:02:33 +0530

    task: intial setup

---
+++ school_server/settings/settings_stage.py
@@ -0,0 +1,16 @@
+from school_server.settings.settings import *
+
+ENVIRONMENT = "Stage"
+print(f"{ENVIRONMENT} environment")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
